be9863987a9ad0df495e947673bc07c4c181ccae
Kevin Litwack <kevin.litwack@gmail.com>
2013-08-13 02:51:01 -0700

    Added Program and PixelMatrix interfaces, and moved ColorCycle logic from sketch into program

--- processing_library/src/org/playasophy/wonderdome/Wonderdome.java
+++ processing_library/src/org/playasophy/wonderdome/Wonderdome.java
@@ -10,141 +10,160 @@ import processing.core.*;
 import com.heroicrobot.dropbit.registry.*;
 import com.heroicrobot.dropbit.devices.pixelpusher.Strip;
 
+import org.playasophy.wonderdome.program.ColorCycle;
+import org.playasophy.wonderdome.program.Program;
+
 
 public class Wonderdome {
 
-  private final PApplet parent;
-  private DeviceRegistry registry;
-  private DeviceObserver devices;
-  private List<Strip> strips;
-  private List<SimulatedStrip> simulatedStrips;
-  private int numStrips;
-  private int pixelsPerStrip;
+    private final PApplet parent;
+    private DeviceRegistry registry;
+    private DeviceObserver devices;
+    private List<Strip> strips;
+    private SimulatedPixelMatrix simulation;
+    private Program currentProgram;
 
-  public Wonderdome(PApplet parent) {
-    this.parent = parent;
-    parent.registerMethod("draw", this);
-    parent.registerMethod("dispose", this);
-    initHardware();
-    initSimulation();
-  }
+    // Profiling variables.
+    private int cycles = 0;
+    private int profileCycles = 100;
+    private long gfxElapsed = 0L;
+    private long pixelElapsed = 0L;
+    private long totalElapsed = 0L;
 
 
 
-  ///// ACCESSORS/MUTATORS /////
+    ///// INITIALIZATION /////
 
-  public int getNumStrips() {
-    return numStrips;
-  }
-
-  public int getPixelsPerStrip() {
-    return pixelsPerStrip;
-  }
-
-
-
-  ///// PUBLIC METHODS /////
-
-  public void draw() {
-    //long gfxStart = System.currentTimeMillis();
-    if ( isSimulated() ) {
-      for ( int s = 0; s < simulatedStrips.size(); s++ ) {
-        simulatedStrips.get(s).draw();
-      }
-    } else {
-      strips = registry.getStrips();
+    public Wonderdome(PApplet parent) {
+        this.parent = parent;
+        parent.registerMethod("draw", this);
+        parent.registerMethod("dispose", this);
+        initHardware();
+        simulation = new SimulatedPixelMatrix(parent);
+        // TODO: Don't hardcode the program.
+        currentProgram = new ColorCycle(parent);
     }
-    //gfxElapsed += System.currentTimeMillis() - gfxStart;
-  }
 
-  public void setPixel(int strip, int pixel, int color) {
-    if ( isSimulated() ) {
-      simulatedStrips.get(strip).setPixel(color, pixel);
-    } else {
-      strips.get(strip).setPixel(color, pixel);
+
+
+    ///// PUBLIC METHODS /////
+
+    public void draw() {
+
+        long totalStart = System.currentTimeMillis();
+        if ( isSimulated() ) {
+            long gfxStart = System.currentTimeMillis();
+            simulation.draw();
+            gfxElapsed += System.currentTimeMillis() - gfxStart;
+        } else {
+            long pixelStart = System.currentTimeMillis();
+            strips = registry.getStrips();
+            pixelElapsed += System.currentTimeMillis() - pixelStart;
+        }
+        totalElapsed += System.currentTimeMillis() - totalStart;
+
+        // TODO: Only perform profiling if requested via environment variable (or some other
+        // runtime-configurable state).
+        updateProfiling();
+
     }
-  }
 
-  public void dispose() {
-    // Anything in here will be called automatically when 
-    // the parent sketch shuts down. For instance, this might
-    // shut down a thread used by this library.
-  }
-
-
-
-  ///// PRIVATE METHODS /////
-
-  private void initHardware() {
-    registry = new DeviceRegistry();
-    devices = new DeviceObserver();
-    registry.addObserver(devices);
-  }
-
-  private void initSimulation() {
-    if ( isSimulated() ) {
-
-      // Set the window size to a reasonably large display.
-      parent.size(300, 1000);
-
-      numStrips = 6;
-      pixelsPerStrip = 240;
-      simulatedStrips = new ArrayList<SimulatedStrip>(numStrips);
-
-      int stripWidth = parent.width / numStrips;
-      for ( int i = 0; i < numStrips; i++ ) {
-        SimulatedStrip strip = new SimulatedStrip(parent, (i * stripWidth) + (stripWidth / 2), 0, pixelsPerStrip);
-        simulatedStrips.add(strip);
-      }
-
-    } else {
-      parent.size(5, 5);
-      simulatedStrips = null;
+    public void dispose() {
+        // Anything in here will be called automatically when 
+        // the parent sketch shuts down. For instance, this might
+        // shut down a thread used by this library.
     }
-  }
 
-  private boolean isSimulated() {
-    return !devices.present;
-  }
+    public PixelMatrix getPixelMatrix() {
+        if ( isSimulated() ) {
+            return simulation;
+        } else {
+            // TODO: return a hardware PixelMatrix.
+            throw new UnsupportedOperationException("Hardware PixelMatrix not implemented yet");
+        }
+    }
+
+    public Program getProgram() {
+        return currentProgram;
+    }
 
 
 
-  ///// PRIVATE CLASSES /////
+    ///// PRIVATE METHODS /////
 
-  // Observer class for hardware detection.
-  // TODO: Split out into separate source file? (No good reason for this to be an inner class...)
-  class DeviceObserver implements Observer {
-    public boolean present = false;
+    private void initHardware() {
+        registry = new DeviceRegistry();
+        devices = new DeviceObserver();
+        registry.addObserver(devices);
+    }
 
-    @Override
-    public void update(final Observable target, final Object device) {
+    private boolean isSimulated() {
+        return !devices.present;
+    }
 
-      DeviceRegistry registry = (DeviceRegistry) target;
+    private void updateProfiling() {
 
-      if ( device != null ) {
-        parent.println("Updated device: " + device);
+        // Report profiling.
+        if ( cycles % profileCycles == 0 ) {
 
-        if ( !present ) {
-          parent.println("Initializing device registry and starting pushing");
-          registry.startPushing();
-          registry.setExtraDelay(0);
-          registry.setAutoThrottle(true);
+            System.out.printf("cycle %d: ~%d ms/cycle (%.2f cps), %d ms gfx (%.2f%%), %d ms pixels (%.2f%%)\n",
+                cycles,
+                totalElapsed / profileCycles,
+                1000.0 * profileCycles / totalElapsed,
+                gfxElapsed / profileCycles,
+                100.0 * gfxElapsed / totalElapsed,
+                pixelElapsed / profileCycles,
+                100.0 * pixelElapsed / totalElapsed);
+
+            gfxElapsed = 0L;
+            pixelElapsed = 0L;
+            totalElapsed = 0L;
+
         }
 
-        present = true;
-      } else {
-        List<Strip> strips = registry.getStrips();
-
-        if ( present && strips.isEmpty() ) {
-          parent.println("Stopping pushing");
-          registry.stopPushing();
-        }
-
-        present = !strips.isEmpty();
-      }
+        cycles++;
 
     }
 
-  }
+
+
+
+    ///// PRIVATE CLASSES /////
+
+    // Observer class for hardware detection.
+    // TODO: Split out into separate source file? (No good reason for this to be an inner class...)
+    class DeviceObserver implements Observer {
+        public boolean present = false;
+
+        @Override
+        public void update(final Observable target, final Object device) {
+
+            DeviceRegistry registry = (DeviceRegistry) target;
+
+            if ( device != null ) {
+                parent.println("Updated device: " + device);
+
+                if ( !present ) {
+                    parent.println("Initializing device registry and starting pushing");
+                    registry.startPushing();
+                    registry.setExtraDelay(0);
+                    registry.setAutoThrottle(true);
+                }
+
+                present = true;
+            } else {
+                List<Strip> strips = registry.getStrips();
+
+                if ( present && strips.isEmpty() ) {
+                    parent.println("Stopping pushing");
+                    registry.stopPushing();
+                }
+
+                present = !strips.isEmpty();
+            }
+
+        }
+
+    }
 
 }
