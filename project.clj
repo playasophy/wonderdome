ead5b5566afc8852d9a4542bda7efb1c10aaf769
Greg Look <greg@greg-look.net>
2013-09-16 17:25:51 -0700

    Add initial leininen project definition.

---
+++ project.clj
@@ -0,0 +1,8 @@
+;; Wonderdome project definition
+(defproject com.mvxcvi/wonderdome "1.0.0-SNAPSHOT"
+  :description "Control and rendering software for driving an LED strip art project with various visualizations."
+  :url "https://github.com/mvxcvi/wonderdome"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+
+  :dependencies [[org.clojure/clojure "1.5.1"]])
