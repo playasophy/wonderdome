d189e9e0f52c157955a2b4612458ab9590d58cde
Greg Look <greg@greg-look.net>
2014-07-08 00:01:42 -0700

    Remove old notes in wonderdome.core ns.

--- src/org/playasophy/wonderdome/core.clj
+++ src/org/playasophy/wonderdome/core.clj
@@ -2,34 +2,21 @@
   (:require
     [com.stuartsierra.component :as component]))
 
-;; Modes have some state, probably stored in an atom.
-;; Displays are protocol objects which can be read(?) and written to, but still side effecting?
-;; Loop reads in events, calls mode update.
 
-;; There also needs to be some concept of pixel layout.
-;; Does it make sense for a mode to be written for multiple layouts? Probably not.
-;; All layouts may address pixels linearly, but individual layouts should have
-;; more appropriate addressing. For example, square grids are best addressed
-;; using (x, y) coordinates, while a geodesic grid... uh, TBD. (strut, index)?
-
-;; A display can map a pixel index to various properties:
-;; SquareGrid: x, y
-;; Geodesic3V: strut, index, azimuth, elevation
-
-
+#_
 (def wonderdome-state
-  "Complete representation of the current wonderdome state."
-  (atom {:state :running
-         :last-autocycle 1234567890
-         :playlist [:colorcycle
-                    :sweep
-                    :twinkle
-                    :marqee]
-         :modes {:colorcycle {:speed 0.1
-                              :density 2.6
-                              :offset 0.3208}
-                 :sweep {:offset "..."}}
-         :event-handlers []}))
+  "Example representation of the current wonderdome state."
+  {:state :running
+   :last-autocycle #inst "2014-07-07T11:03:42Z"
+   :playlist [:colorcycle  ; queue is probably better
+              :sweep
+              :twinkle
+              :marqee]
+   :modes {:colorcycle {:speed 0.1
+                        :density 2.6
+                        :offset 0.3208}
+           :sweep {:offset "..."}}
+   :event-handlers []})
 
 
 (defn initialize
