86a4ec336e88b0634c9be21741c6e85decad4166
Greg Look <greg@greg-look.net>
2014-07-16 13:29:18 -0700

    Use java.awt.Color instead of re-implementing conversions.

--- src/org/playasophy/wonderdome/util/color.clj
+++ src/org/playasophy/wonderdome/util/color.clj
@@ -1,90 +1,51 @@
 (ns org.playasophy.wonderdome.util.color
   "Functions for manipulating and generating color values.
-
-  See: http://en.wikipedia.org/wiki/HSL_and_HSV")
-
-
-;;;;; HELPER FUNCTIONS ;;;;;
-
-(defn float->byte
-  "Converts a number in the range [0.0, 1.0] to an integer in the range [0,
-  255]. Values outside this range will be clamped."
-  [value]
-  {:pre [(number? value)]}
-  (-> value (* 255) int (bit-and 0xFF)))
-
-
-(defn byte->float
-  "Converts a byte in the range [0, 255] to a floating-point value in the range
-  [0.0, 1.0]."
-  [value]
-  (-> value (bit-and 0xFF) (/ 255.0)))
-
+  See: http://en.wikipedia.org/wiki/HSL_and_HSV"
+  (import
+    java.awt.Color))
 
 
 ;;;;; RGB COLOR SPACE ;;;;;
 
 (defn rgb*
-  "Creates a color value compatible with Processing and PixelPusher hardware.
-  Each color channel should be an 8-bit value from 0 to 255. If not set, the
-  alpha channel defaults to full opacity (255)."
-  ([r g b]
-   (rgb* r g b 255))
-  ([r g b a]
-   {:pre [(integer? r) (integer? g) (integer? b) (integer? a)]}
-   (unchecked-int
-     (+ (bit-shift-left (bit-and 0xFF a) 24)
-        (bit-shift-left (bit-and 0xFF r) 16)
-        (bit-shift-left (bit-and 0xFF g)  8)
-                        (bit-and 0xFF b)))))
+  "Creates a color value from red, green, and blue component channels. Each
+  component should be an 8-bit value from [0, 255]."
+  [r g b]
+  (.getRGB (Color. (int r) (int g) (int b))))
 
 
 (defn rgb
-  "Creates a color value from red, green, and blue channel values. Each value
-  should be a number from [0.0, 1.0]."
+  "Creates a color value from red, green, and blue component channels. Each
+  component should be a number from [0.0, 1.0]."
   [r g b]
-  {:pre [(number? r) (number? g) (number? b)]}
-  (rgb*
-    (float->byte r)
-    (float->byte g)
-    (float->byte b)))
+  (.getRGB (Color. (float r) (float g) (float b))))
 
 
-(defn red
-  "Extracts the red channel from a color value. Returns a floating-point value
-  from [0.0, 1.0]."
+(defn rgb-components
+  "Takes a color value and returns a vector containing the red, green, and blue
+  component channels as floating-point numbers."
   [color]
-  {:pre [(integer? color)]}
-  (-> color (bit-shift-right 16) byte->float))
-
-
-(defn green
-  "Extracts the green channel from a color value. Returns a floating-point
-  value from [0.0, 1.0]."
-  [color]
-  {:pre [(integer? color)]}
-  (-> color (bit-shift-right 8) byte->float))
-
-
-(defn blue
-  "Extracts the blue channel from a color value. Returns a floating-point value
-  from [0.0, 1.0]."
-  [color]
-  {:pre [(integer? color)]}
-  (byte->float color))
+  (vec (.getRGBColorComponents (Color. (unchecked-int color)) nil)))
 
 
 
 ;;;;; HSV COLOR SPACE ;;;;;
 
 (defn hsv
-  "Creates a color value from a hue, saturation, and value. Hue is an angle in
-  [0.0, 2*PI], and saturation and value should be numbers from [0.0, 1.0]."
+  "Creates a color value from a hue, saturation, and value. The fractional
+  portion of the hue determines the angle in the HSV space; saturation and
+  value should be numbers from [0.0, 1.0]."
   [h s v]
-  ; TODO: implement HSV -> RGB conversion
-  nil)
+  ; TODO: document hue angles for pure red/green/blue
+  (Color/HSBtoRGB (float h) (float s) (float v)))
 
 
+(defn hsv-components
+  "Takes a color value and returns a vector containing the hue, saturation, and
+  value components as floating-point numbers."
+  [color]
+  (let [c (Color. (unchecked-int color))]
+    (vec (Color/RGBtoHSB (.getRed c) (.getBlue c) (.getGreen c) nil))))
 
 
 
