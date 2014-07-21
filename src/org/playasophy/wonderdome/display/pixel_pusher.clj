8b4db92267aa1d1e00042340104d005d199b61f2
Greg Look <greg@greg-look.net>
2014-07-21 14:43:01 -0700

    Flesh out pixel-pusher display a bit more.

--- src/org/playasophy/wonderdome/display/pixel_pusher.clj
+++ src/org/playasophy/wonderdome/display/pixel_pusher.clj
@@ -2,24 +2,53 @@
   (:require
     [com.stuartsierra.component :as component]
     [org.playasophy.wonderdome.display.core :as display])
-  #_
   (:import
-    [com.heroicrobot.dropbit.registry *]
-    com.heroicrobot.dropbit.devices.pixelpusher.Strip))
+    #_
+    (com.heroicrobot.dropbit.devices.pixelpusher
+      Strip)
+    #_
+    (com.heroicrobot.dropbit.registry
+      DeviceRegistry)))
 
 
-;;;;; WONDERDOME DISPLAY ;;;;;
+;;;;; CONFIGURATION ;;;;;
+
+; Suppress spurious log output.
+#_
+(->
+  DeviceRegistry
+  .getName
+  java.util.logging.Logger/getLogger
+  (.setLevel java.util.logger.Level/WARNING))
+
+
+(defn- registry-observer
+  "Constructs a new callback function which will update the strips in the given
+  atom when the device registry changes."
+  [registry strips]
+  (reify java.util.Observer
+    (update [this target device]
+      (let [new-strips (vec (.getStrips registry))]
+        (println "Updated device:" device (str "(" (count new-strips) " strips)"))
+        (reset! strips new-strips)))))
+
+
+
+;;;;; PIXEL PUSHER DISPLAY ;;;;;
 
 (defrecord PixelPusherDisplay
-  [registry strips running])
+  [registry strips running]
 
-(extend-type PixelPusherDisplay
   component/Lifecycle
 
   (start
     [this]
     (println "Starting PixelPusher display...")
-    ; TODO: add device observer
+    (doto registry
+      (.addObserver (registry-observer registry strips))
+      (.startPushing)
+      (.setExtraDelay 0)
+      (.setAutoThrottle true))
     (assoc this :running true))
 
 
@@ -27,6 +56,7 @@
     [this]
     (println "Stopping PixelPusher display...")
     ; TODO: clear strips, remove device observer?
+    (.stopPushing registry)
     (assoc this :running false))
 
 
@@ -38,8 +68,8 @@
     nil))
 
 
-(defn pixel-pusher-display
+(defn pixel-pusher
   "Creates a new display streaming color commands to connected pixel-pusher
   hardware."
-  ([registry]
-   (PixelPusherDisplay. registry (atom []) false)))
+  [registry]
+  (PixelPusherDisplay. registry (atom []) false))
