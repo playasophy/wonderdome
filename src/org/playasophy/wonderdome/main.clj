e94a617a961aa0a9b1342a9a0c831e8167ab7d2a
Greg Look <greg@greg-look.net>
2014-07-25 21:55:38 -0700

    Add config file loading to main ns.

--- src/org/playasophy/wonderdome/main.clj
+++ src/org/playasophy/wonderdome/main.clj
@@ -3,17 +3,14 @@
   (:gen-class)
   (:require
     [clojure.core.async :as async]
+    [clojure.java.io :as io]
+    [clojure.tools.logging :as log]
     [com.stuartsierra.component :as component]
     (org.playasophy.wonderdome
       [state :as state]
       [system :as system])
-    (org.playasophy.wonderdome.display
-      [pixel-pusher :refer [pixel-pusher]])
-    (org.playasophy.wonderdome.geometry
-      [layout :as layout])
     (org.playasophy.wonderdome.input
       [gamepad :as gamepad]
-      [middleware :as middleware]
       [timer :as timer])
     [org.playasophy.wonderdome.mode.config :as modes]))
 
@@ -21,6 +18,25 @@
 ; Force java.util.logging through SLF4J/Logback
 (org.slf4j.bridge.SLF4JBridgeHandler/removeHandlersForRootLogger)
 (org.slf4j.bridge.SLF4JBridgeHandler/install)
+
+
+(defn- load-config-file
+  "Reads a Clojure configuration file. The given require forms will be loaded
+  and made available to the configuration."
+  [path & requirements]
+  (let [file (io/file path)]
+    (when (.exists file)
+      (let [temp-ns (gensym)]
+        (try
+          (binding [*ns* (create-ns temp-ns)]
+            (clojure.core/refer-clojure)
+            (when (seq requirements)
+              (apply require requirements))
+            (load-string (slurp path)))
+          (catch Exception e
+            (log/error e (str "Error loading config file: " path))
+            nil)
+          (finally (remove-ns temp-ns)))))))
 
 
 (def dimensions
@@ -31,35 +47,28 @@
    :strips 6})
 
 
-(defn -main [& args]
-  (let [{:keys [timer-period]
-         :or {timer-period 30}}
-        (map read-string args)]
-    (->
-      {:layout
-       (layout/star dimensions)
+(defn -main [& [config-path]]
+  (->
+    (load-config-file
+      config-path
+      '[org.playasophy.wonderdome.state :as state]
+      '[org.playasophy.wonderdome.display.pixel-pusher :refer [pixel-pusher]]
+      '[org.playasophy.wonderdome.geometry.layout :as layout]
+      '[org.playasophy.wonderdome.input.middleware :as middleware])
 
-       :display
-       (pixel-pusher)
+    ; TODO: load some kind of saved state
+    (assoc :initial-state
+      (state/initialize modes/config))
 
-       :handler
-       (-> state/update-mode
-           middleware/mode-selector
-           (middleware/autocycle-modes (comp #{:button/press :button/repeat} :type)))
+    system/initialize
 
-       ; TODO: load some kind of saved state
-       :initial-state
-       (state/initialize modes/config)}
+    (system/add-input :timer timer/timer
+      (async/chan (async/dropping-buffer 3))
+      30)
 
-      system/initialize
+    (system/add-input :gamepad gamepad/snes
+      (async/chan (async/dropping-buffer 10)))
 
-      (system/add-input :timer timer/timer
-        (async/chan (async/dropping-buffer 3))
-        timer-period)
+    ; TODO: audio parser
 
-      (system/add-input :gamepad gamepad/snes
-        (async/chan (async/dropping-buffer 10)))
-
-      ; TODO: audio parser
-
-      component/start)))
+    component/start))
