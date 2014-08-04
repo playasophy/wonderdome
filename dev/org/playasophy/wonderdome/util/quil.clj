b27a238a9f1361deca0ce6fd1f90c0e3a8dd6a7b
Greg Look <greg@greg-look.net>
2014-08-03 23:32:37 -0700

    Fix screen-scale calculations in util.quil dev ns.

--- dev/org/playasophy/wonderdome/util/quil.clj
+++ dev/org/playasophy/wonderdome/util/quil.clj
@@ -6,31 +6,40 @@
     [quil.core :as quil]))
 
 
+(defn screen-scale
+  "Determines how much to scale distances by based on how big the processing
+  display is."
+  ([]
+   (/ (min (quil/width) (quil/height)) 10.0))
+  ([v]
+   (* v (screen-scale))))
+
+
 (defn scale-point
   "Scales up a point vector to screen space."
   [p]
-  (let [s (/ (min (quil/width) (quil/height)) 10.0)]
-    (cartesian/scale p s)))
+  (cartesian/scale p (screen-scale)))
 
 
 (defn draw-axes
   [length]
   (quil/stroke-weight 1)
-  ; x-axis red
-  (quil/stroke (quil/color 255 0 0))
-  (quil/line [0 0 0] (scale-point [length 0 0]))
-  ; y-axis green
-  (quil/stroke (quil/color 0 255 0))
-  (quil/line [0 0 0] (scale-point [0 length 0]))
-  ; z-axis blue
-  (quil/stroke (quil/color 0 0 255))
-  (quil/line [0 0 0] (scale-point [0 0 length])))
+  (let [l (screen-scale length)]
+    ; x-axis red
+    (quil/stroke (quil/color 255 0 0))
+    (quil/line [0 0 0] [l 0 0])
+    ; y-axis green
+    (quil/stroke (quil/color 0 255 0))
+    (quil/line [0 0 0] [0 l 0])
+    ; z-axis blue
+    (quil/stroke (quil/color 0 0 255))
+    (quil/line [0 0 0] [0 0 l])))
 
 
 (defn draw-ground
   [radius]
   (let [c (quil/color 210 200 175)
-        r (* scale radius)]
+        r (screen-scale radius)]
     (quil/fill c)
     (quil/stroke c)
     (quil/ellipse 0 0 r r)))
