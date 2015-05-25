180cc21131afcb427032eabe0796da660dc68ed1
Luis Ballesteros <lballest@lballest-macbookpro.roam.corp.google.com>
2015-05-25 16:59:18 -0700

    Fixing style in new strip_eq mode.

--- src/playasophy/wonderdome/mode/strip_eq.clj
+++ src/playasophy/wonderdome/mode/strip_eq.clj
@@ -2,14 +2,12 @@
   (:require
     [playasophy.wonderdome.geometry.sphere :as sphere]
     [playasophy.wonderdome.mode.core :as mode]
-    [playasophy.wonderdome.util.color :as color]
-  )
-)
+    [playasophy.wonderdome.util.color :as color]))
 
-; StripEQ will process audio signals and turn each strip into an indicator for
-; the strength of a range of frequencies over time.
+;; StripEQ will process audio signals and turn each strip into an indicator for
+;; the strength of a range of frequencies over time.
 (defrecord StripEqMode
-  [num_strips pix_per_strip]
+  [num-strips pix-per-strip]
 
   mode/Mode
 
@@ -18,31 +16,26 @@
     (case (:type event)
       :time/tick
       (assoc this
-             ; TODO: Vary colorindex over time smoothly.
-             :colorindex (double (/ 1.0 num_strips))
-      )
+             ;; TODO: Vary colorindex over time smoothly.
+             :colorindex (double (/ 1.0 num-strips)))
 
       :audio/beat
       (assoc this
-             ; TODO: Reserve one strip for beats?
+             ;; TODO: Reserve one strip for beats?
              :beat/at (System/currentTimeMillis)
-             :beat/power (:power event)
-      )
+             :beat/power (:power event))
 
       :audio/freq
       (assoc this
              :freq/at (System/currentTimeMillis)
-             ; Break frequencies into groups per strip and take mean for each.
-             :freq/group_mean (map
-               #(/ (reduce + %) (count %))
-               (partition
-                 (int (/ (count (:spectrum event)) num_strips))
-                 (:spectrum event)
-               )
-             )
+             ;; Break frequencies into groups per strip and take mean for each.
+             :freq/group_mean
+             (->> (:spectrum event)
+                  (partition (-> event :spectrum count (/ num-strips) int))
+                  (map #(/ (reduce + %) (count %))))
       )
 
-      ; default
+      ;; Default case.
       this
     )
   )
@@ -51,16 +44,16 @@
   (render
     [this pixel]
     (let [
-      ; Frequency for this strip from most recent signal.
-      ; Assumes frequencies range from 0 to 1.
+      ;; Frequency for this strip from most recent signal.
+      ;; Assumes frequencies range from 0 to 1.
       freq (nth (:freq/group_mean this) (:strip pixel))
-      ; How far down the strip is this pixel?
-      ; Ranges from 0 (start of strip) to 1 (end of strip).
-      pix_to_len_pct (double (/ (inc (:pixel pixel)) (inc pix_per_strip)))
+      ;; How far down the strip is this pixel?
+      ;; Ranges from 0 (start of strip) to 1 (end of strip).
+      pix-to-len-pct (double (/ (inc (:pixel pixel)) (inc pix-per-strip)))
       ]
-      (if (< pix_to_len_pct (or freq 0.0))
-        ; Each strip has the same color,
-        ; but strips form an even spread over the color spectrum.
+      (if (< pix-to-len-pct (or freq 0.0))
+        ;; Each strip has the same color,
+        ;; but strips form an even spread over the color spectrum.
         (color/rainbow (* (:strip pixel) (:colorindex this)))
         color/none
       )
