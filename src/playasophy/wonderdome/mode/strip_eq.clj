b1775d33a050e725d292abdc98e1ca1f54406f8c
Greg Look <greg@greg-look.net>
2015-07-03 16:46:13 -0700

    Clean up strip-eq formatting.

--- src/playasophy/wonderdome/mode/strip_eq.clj
+++ src/playasophy/wonderdome/mode/strip_eq.clj
@@ -4,6 +4,7 @@
     [playasophy.wonderdome.mode.core :as mode]
     [playasophy.wonderdome.util.color :as color]))
 
+
 ;; StripEQ will process audio signals and turn each strip into an indicator for
 ;; the strength of a range of frequencies over time.
 (defrecord StripEqMode
@@ -29,37 +30,28 @@
       (assoc this
              :freq/at (System/currentTimeMillis)
              ;; Break frequencies into groups per strip and take mean for each.
-             :freq/group_mean
+             :freq/group-mean
              (->> (:spectrum event)
                   (partition (-> event :spectrum count (/ num-strips) int))
-                  (map #(/ (reduce + %) (count %))))
-      )
+                  (map #(/ (reduce + %) (count %)))))
 
       ;; Default case.
-      this
-    )
-  )
+      this))
 
 
   (render
     [this pixel]
-    (let [
-      ;; Frequency for this strip from most recent signal.
-      ;; Assumes frequencies range from 0 to 1.
-      freq (nth (:freq/group_mean this) (:strip pixel))
-      ;; How far down the strip is this pixel?
-      ;; Ranges from 0 (start of strip) to 1 (end of strip).
-      pix-to-len-pct (double (/ (inc (:pixel pixel)) (inc pix-per-strip)))
-      ]
+    (let [; Frequency for this strip from most recent signal.
+          ; Assumes frequencies range from 0 to 1.
+          freq (nth (:freq/group-mean this) (:strip pixel))
+          ; How far down the strip is this pixel?
+          ; Ranges from 0 (start of strip) to 1 (end of strip).
+          pix-to-len-pct (double (/ (inc (:pixel pixel)) (inc pix-per-strip)))]
+      ; Each strip has the same color, but strips form an even spread over the
+      ; color spectrum.
       (if (< pix-to-len-pct (or freq 0.0))
-        ;; Each strip has the same color,
-        ;; but strips form an even spread over the color spectrum.
         (color/rainbow (* (:strip pixel) (:colorindex this)))
-        color/none
-      )
-    )
-  )
-)
+        color/none))))
 
 
 (defn init
