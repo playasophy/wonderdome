5c3246ddb2f0ac7950749c9adb94227a34e35205
Kevin Litwack <kevin.litwack@gmail.com>
2015-07-17 18:16:43 -0700

    Add bombs

--- src/clojure/playasophy/wonderdome/mode/bombs.clj
+++ src/clojure/playasophy/wonderdome/mode/bombs.clj
@@ -16,16 +16,35 @@
     (Math/sqrt (+ (Math/pow vertical-distance 2) (Math/pow circle-distance 2)))))
 
 
+(defn- overlay
+  ([layers]
+   (reduce (fn [c1 c2] (if (= c2 color/none) c1 c2)) layers)))
+
+(defn- bomb-color
+  [barrel-radius pixel bomb]
+  (do
+    #_(log/warn pixel bomb)
+    (let [delta (distance barrel-radius (:center bomb) (:barrel pixel))
+          radius (* (:age bomb) 0.00003)]
+      (if (<= delta radius)
+        (color/rainbow (/ delta radius))
+        color/none))))
+
 (defrecord BombsMode
-  [barrel-radius barrel-height cursor-position]
+  [barrel-radius barrel-height cursor-position bombs]
 
   mode/Mode
 
   (update
     [this event]
     (condp = [(:type event) (:input event)]
-      [:button/press :L]
-      this
+      [:time/tick nil]
+      (let [elapsed (or (:elapsed event) 0.0)]
+        (assoc this :bombs
+          (map (fn [bomb]
+                 (let [age' (+ (:age bomb) elapsed)]
+                   (assoc bomb :age age')))
+               (filter #(< (:age %) 10000) bombs))))
 
       [:axis/direction :y-axis]
       (assoc-in this [:cursor-position :z]
@@ -35,8 +54,7 @@
                              :max-val barrel-height))
 
       [:axis/direction :x-axis]
-      (assoc-in
-        this [:cursor-position :theta]
+      (assoc-in this [:cursor-position :theta]
         (let [theta' (control/adjust-wrapped
                        (:theta cursor-position) event
                        :rate sphere/pi
@@ -44,13 +62,21 @@
                        :max-val sphere/tau)]
           (if (>= theta' sphere/tau) 0.0 theta')))
 
+      [:button/press :A]
+      (assoc-in this [:bombs]
+        (conj bombs {:center cursor-position :age 0}))
+
       this))
 
 
   (render
     [this pixel]
-    (let [delta (distance barrel-radius (:barrel pixel) cursor-position)]
-      (if (< delta 0.04) (color/gray 1.0) color/none))))
+    (do
+      #_(log/warn pixel bombs)
+      (let [bomb-colors (map (partial bomb-color barrel-radius pixel) bombs)
+            cursor-delta (distance barrel-radius (:barrel pixel) cursor-position)
+            cursor-color (if (< cursor-delta 0.04) (color/gray 1.0) color/none)]
+        (overlay (conj bomb-colors cursor-color))))))
 
 
 (defn init
@@ -58,4 +84,4 @@
   ([]
    (init 0.099 (* 0.068 8) {:theta 0 :z 0.20}))
   ([barrel-radius barrel-height cursor-position]
-   (BombsMode. barrel-radius barrel-height cursor-position)))
+   (BombsMode. barrel-radius barrel-height cursor-position [])))
