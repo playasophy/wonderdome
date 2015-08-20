7eabf2c756e38526d4cb9f710b9bf39472177153
Greg Look <greg@greg-look.net>
2015-08-20 12:52:25 -0700

    Don't use admin user to deploy.

--- util/deploy.sh
+++ util/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
-host=wonderdome
+# Note that this relies on the wonder user being able to control the wonderdome
+# service without a password. This requires a sudoers directive like:
+#
+#     wonder  ALL=(root) NOPASSWD:/usr/sbin/service wonderdome *
 
-admin_user=greg
-admin=$admin_user@$host
+host=wonderdome
 
 srv_user=wonder
 srv=$srv_user@$host
@@ -20,7 +22,7 @@ echo "Backing up deployed jar..."
 ssh $srv "cp /srv/wonder/wonderdome.jar /srv/wonder/wonderdome.jar.bak"
 
 echo "Stopping service..."
-ssh $admin "sudo service wonderdome stop"
+ssh $srv "sudo service wonderdome stop"
 
 echo "Deploying uberjar..."
 scp target/uberjar/$jar_name-*-standalone.jar $srv_home/wonderdome.jar
@@ -32,4 +34,4 @@ echo "Deploying configuration..."
 scp $config_file $srv_home/$config_file
 
 echo "Restarting service..."
-ssh $admin "sudo service wonderdome start"
+ssh $srv "sudo service wonderdome start"
