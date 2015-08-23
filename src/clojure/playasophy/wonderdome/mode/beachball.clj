b5a289a96f463e555a2e31e38c4c763568135e1f
Kevin Litwack <kevin.litwack@gmail.com>
2015-08-22 23:18:31 -0700

    Bump up beachball speed adjust rate

--- src/clojure/playasophy/wonderdome/mode/beachball.clj
+++ src/clojure/playasophy/wonderdome/mode/beachball.clj
@@ -37,7 +37,7 @@
 
       [:axis/direction :y-axis]
       (assoc this :speed (control/adjust speed event
-                                         :rate (/ sphere/tau 12)
+                                         :rate (/ sphere/tau 6)
                                          :min-val (first speed-bounds)
                                          :max-val (second speed-bounds)))
 
