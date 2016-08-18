c9ee51cedfb6d937ed50d581e8867dc511342f9b
Greg Look <greg@greg-look.net>
2016-08-17 22:16:49 -0700

    Wire up new SNES controller.

--- src/clojure/playasophy/wonderdome/input/gamepad.clj
+++ src/clojure/playasophy/wonderdome/input/gamepad.clj
@@ -86,19 +86,19 @@
 
 
 
-;;;;; SNES CONTROLLER ;;;;;
+;;;;; ORIGINAL SNES CONTROLLER ;;;;;
 
-(def ^:const snes-vendor-id  0x12bd)
-(def ^:const snes-product-id 0xd015)
+(def ^:const og-snes-vendor-id  0x12bd)
+(def ^:const og-snes-product-id 0xd015)
 
-(def ^:private snes-axes
+(def ^:private og-snes-axes
   {:x-axis 0.0
    :y-axis 0.0})
 
-(def ^:private snes-buttons
+(def ^:private og-snes-buttons
   #{:X :A :B :Y :L :R :select :start :left :right :up :down})
 
-(def ^:private snes-bits
+(def ^:private og-snes-bits
   {:X      [3 0]
    :A      [3 1]
    :B      [3 2]
@@ -109,7 +109,7 @@
    :start  [4 1]})
 
 
-(defn- snes-read-state
+(defn- og-snes-read-state
   "SNES controller state is read 8 bytes at a time:
     byte 0: x-axis [00 7F FF] (FF is right)
     byte 1: y-axis [00 7F FF] (FF is down)
@@ -124,6 +124,81 @@
            (apply str (map (partial format "%02X") (take len buffer)))))
     (let [x-axis (byte-axis (aget buffer 0))
           y-axis (- (byte-axis (aget buffer 1)))]
+      (assoc
+        (read-buttons og-snes-bits buffer)
+        :x-axis x-axis
+        :y-axis y-axis
+        :left  (neg? x-axis)
+        :right (pos? x-axis)
+        :down  (neg? y-axis)
+        :up    (pos? y-axis)))))
+
+
+(defn- og-snes-state-events
+  [old-state new-state elapsed]
+  (remove nil?
+    (concat
+      (axis-direction-events og-snes-axes old-state elapsed)
+      (button-hold-events og-snes-buttons old-state elapsed)
+      (button-state-events og-snes-buttons old-state new-state))))
+
+
+(defn og-snes
+  "Creates a new SNES gamepad input which will send button events to the given
+  channel."
+  [channel]
+  {:pre [(some? channel)]}
+  ; TODO: sometimes nil when refreshing code...
+  (if-let [device (usb/find-device og-snes-vendor-id og-snes-product-id)]
+    (usb/hid-input
+      :snes
+      channel
+      device
+      og-snes-read-state
+      og-snes-state-events)))
+
+
+
+;;;;; NEW SNES CONTROLLER ;;;;;
+
+(def ^:const snes-vendor-id  2064)
+(def ^:const snes-product-id 58625)
+
+(def ^:private snes-axes
+  {:x-axis 0.0
+   :y-axis 0.0})
+
+(def ^:private snes-buttons
+  #{:X :A :B :Y :L :R :select :start :left :right :up :down})
+
+(def ^:private snes-bits
+  {:X      [5 4]
+   :A      [5 5]
+   :B      [5 6]
+   :Y      [5 7]
+   :L      [6 0]
+   :R      [6 1]
+   :select [6 4]
+   :start  [6 5]})
+
+
+(defn- snes-read-state
+  "SNES controller state is read 8 bytes at a time:
+    byte 0: always 01
+    byte 1: always 80
+    byte 2: always 80
+    byte 3: x-axis [00 7F FF] (FF is right)
+    byte 4: y-axis [00 7F FF] (FF is down)
+    byte 5: 0F + X (10), A (20), B (40), Y (80)
+    byte 6: L (1), R (2), select (10), start (20)
+    byte 7: always 00"
+  [^bytes buffer len]
+  (if (< len 8)
+    (log/warn
+      (str "Incomplete data read from SNES controller: "
+           (apply str (map (partial format "%02X") (take len buffer)))))
+    (let [x-axis (byte-axis (aget buffer 3))
+          y-axis (- (byte-axis (aget buffer 4)))]
       (assoc
         (read-buttons snes-bits buffer)
         :x-axis x-axis
